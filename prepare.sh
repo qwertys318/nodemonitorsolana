475acd999e174003445276ccb877d52fcd0e068b
qwertys318 <343604@gmail.com>
2024-05-22 17:59:12 +0700

    save

--- prepare.sh
+++ prepare.sh
@@ -6,8 +6,6 @@ sudo mkdir /var/log/nodemonitor
 sudo chown sol:sol /var/log/nodemonitor
 chmod 755 /var/log/nodemonitor
 sudo cp ./nodemonitor.service /etc/systemd/system/nodemonitor.service
-chmod +x ./nodemonitor.sh
-cp ./nodemonitor.sh ~/
 sudo systemctl daemon-reload
 sudo systemctl enable --now nodemonitor
-echo "CHECK THE RPC IN ~/nodemonitor.sh"
+echo "CHECK THE RPC IN .service"
